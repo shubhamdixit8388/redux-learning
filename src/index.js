030cb950e733c5d5e4e41bd2d391932fb4ccebe9
Shubham <svdixit8388@gmail.com>
2021-10-05 21:43:51 +0530

    added projects reducer as a exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import configureStore from "./store/configure-store";
 import customStore from "./store/custom-store";
 import * as actions from "./store/bugs";
+import { addProject } from "./store/projects";
 
 const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store Changed:", store.getState());
 });
+
+store.dispatch(addProject({ name: "Project 1" }));
 
 store.dispatch(actions.addBug({ description: "Bug 1" }));
 store.dispatch(actions.resolveBug({ id: 1 }));
