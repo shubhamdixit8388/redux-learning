3fb998a1a4a34eac794cec0cab24d00fa9903eb7
Shubham <svdixit8388@gmail.com>
2021-10-05 21:30:46 +0530

    creating slices

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,37 +1,33 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// // Action types
-// const BUG_ADDED = "BUG_ADDED";
-// const BUG_REMOVE = "BUG_REMOVE";
-// const BUG_RESOLVE = "BUG_RESOLVE";
-// const BUG_UPDATE = "BUG_UPDATE";
-
-// Action creators
-export const addBug = createAction("BUG_ADDED");
-export const removeBug = createAction("BUG_REMOVE");
-export const resolveBug = createAction("BUG_RESOLVE");
-export const updateBug = createAction("BUG_UPDATE");
+import { createSlice } from "@reduxjs/toolkit";
 
 let lastBugId = 0;
-export default createReducer([], {
-  [addBug.type]: (state, action) => {
-    state.push({
-      id: ++lastBugId,
-      description: action.payload.description,
-      isResolved: false,
-    });
-  },
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // action: action handlers
+    addBug: (bugs, action) => {
+      bugs.push({
+        id: ++lastBugId,
+        description: action.payload.description,
+        isResolved: false,
+      });
+    },
 
-  [removeBug.type]: (state, action) => {
-    return state.filter((bug) => bug.id !== action.payload.id);
-  },
+    removeBug: (state, action) => {
+      return state.filter((bug) => bug.id !== action.payload.id);
+    },
 
-  [removeBug.type]: (state, action) => {
-    return state.filter((bug) => bug.id !== action.payload.id);
-  },
+    resolveBug: (state, action) => {
+      const index = state.findIndex((bug) => bug.id === action.payload.id);
+      state[index].isResolved = true;
+    },
 
-  [resolveBug.type]: (state, action) => {
-    const index = state.findIndex((bug) => bug.id === action.payload.id);
-    state[index].isResolved = true;
+    updateBug: (state, action) => {
+      return [...state];
+    },
   },
 });
+
+export const { addBug, removeBug, resolveBug, updateBug } = slice.actions;
+export default slice.reducer;
