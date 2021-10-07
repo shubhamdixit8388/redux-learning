fdcae2056ab6c08ab002ac216bc91479b0eb1a28
Shubham <svdixit8388@gmail.com>
2021-10-07 11:07:11 +0530

    creating mock test

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,12 +1,19 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { addBug } from "../bugs";
 import configureStore from "../configure-store";
 
 describe("bugSlice", () => {
   it("should handle the addBug action", async () => {
-    const store = configureStore();
     const bug = { description: "as" };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    console.log("store.getstate:", store.getState());
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    console.log("store.getstate:", store.getState().entities.bugs.list);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
